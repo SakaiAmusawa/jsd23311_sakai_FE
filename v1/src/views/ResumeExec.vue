4ec207291ac9f394637320c8cfde6c6775bba128
sakai <sakai263016@gmail.com>
2024-02-01 20:52:54 +0800

    练习

--- v1/src/views/ResumeExec.vue
+++ v1/src/views/ResumeExec.vue
@@ -1,16 +1,16 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 
-const person = ref({name: "", age: "", imgUrl: "", friends: []});
+const person = ref({ name: "", age: "", imgUrl: "", friends: [] });
 
 const load = () => {
-  person.value = {name: "fcq", age: "20", imgUrl: "fcq.jpg", friends: ["路飞", "索隆"]}
-}
+  person.value = { name: "fcq", age: "20", imgUrl: "fcq.jpg", friends: ["路飞", "索隆"] };
+};
 </script>
 
 <template>
   <h1>Resume</h1>
-  <table border="1px">
+  <table border="1px" cellpadding="8">
     <tr>
       <td>照片</td>
       <td><img :src="person.imgUrl" width="100px"></td>
@@ -27,7 +27,7 @@ const load = () => {
       <td>朋友</td>
       <td>
         <ul>
-          <li v-for="fn in person.friends">{{ fn }}</li>
+          <li v-for="friend in person.friends" :key="friend">{{ friend }}</li>
         </ul>
       </td>
     </tr>
@@ -36,5 +36,17 @@ const load = () => {
 </template>
 
 <style scoped>
+table {
+  margin-top: 20px;
+  border-collapse: collapse;
+}
 
+td {
+  padding: 8px;
+  border: 1px solid #ccc;
+}
+
+img {
+  max-width: 100%;
+}
 </style>
