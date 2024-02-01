9b5bb5d8b6c1f5d4f20c24b6d622536d26dfc380
sakai <sakai263016@gmail.com>
2024-02-01 11:30:09 +0800

    循环指令

--- v1/src/views/ForCmd.vue
+++ v1/src/views/ForCmd.vue
@@ -10,9 +10,9 @@ const arr = ref(["张三", "李四", "王五", "赵六", "钱七", "周八"])
   <ul>
     <li v-for="uname in arr">{{ uname }}</li>
   </ul>
-  <!-- 从1到20遍历，固定写法，默认从1开始，步长为1，结束数字可以自定义 -->
+  <!-- 从1到X遍历，固定写法，默认从1开始，步长为1，结束数字可以自定义 -->
   <ol>
-    <li v-for="item in 20">{{ item }}</li>
+    <li v-for="item in 5">{{ item }}</li>
   </ol>
 </template>
 
