0826bf5e2983112495b92816e9f2b46147d8e403
sakai <sakai263016@gmail.com>
2024-02-01 10:24:10 +0800

    绑定与双向绑定

--- v1/src/views/TwoWaysBind.vue
+++ v1/src/views/TwoWaysBind.vue
@@ -1,9 +1,19 @@
 <script setup>
+import {ref} from "vue";
 
+const info = ref("双向绑定");
 </script>
 
 <template>
+
   <h1>双向绑定</h1>
+  <hr>
+  {{ info }}
+  <input :value="info" type="text">
+  <hr>
+  <!-- 双向绑定的用法 -->
+  <input v-model="info" type="text">
+
 </template>
 
 <style scoped>
