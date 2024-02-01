d5a0ba635da052669d9d653e02d55452eac02370
sakai <sakai263016@gmail.com>
2024-02-01 14:30:22 +0800

    隐藏和显示

--- v1/src/views/HiddenOrDisplay.vue
+++ v1/src/views/HiddenOrDisplay.vue
@@ -1,5 +1,7 @@
 <script setup>
+import {ref} from "vue";
 
+const isShow = ref(true)
 </script>
 
 <template>
@@ -8,6 +10,23 @@
   <p v-if="true">张三</p>
   <p v-if="true">李四</p>
   <p v-if="false">王五</p>
+
+  <hr>
+
+  <!-- v-if="变量" 进一步使用变量来控制元素是否显示 -->
+  <p v-if="isShow">🌙</p>
+  <p v-if="isShow">✨</p>
+  <!-- 与v-if相反 -->
+  <p v-else>☀️</p>
+
+  <hr>
+
+  <!-- v-show是使用display:none；修改元素的显示模式为隐藏 -->
+  <!-- v-if=“false” 是直接在HTML中=删除元素 -->
+  <p v-show="false">小绿</p>
+  <p>小红</p>
+  <!-- 推荐 v-show 开销较小 -->
+
 </template>
 
 <style scoped>
