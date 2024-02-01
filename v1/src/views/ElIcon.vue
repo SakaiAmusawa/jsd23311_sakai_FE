5fe4068e9ba1e606d5ee7d5458c618ab45326e4a
sakai <sakai263016@gmail.com>
2024-02-01 19:17:23 +0800

    icon和message的使用

--- v1/src/views/ElIcon.vue
+++ v1/src/views/ElIcon.vue
@@ -1,11 +1,12 @@
 <script setup>
 
+import {Download, Loading} from "@element-plus/icons-vue";
 </script>
 
 <template>
   <h1>El-icon</h1>
   <el-icon>
-    <el-icon><Loading /></el-icon>
+    <el-icon><download /></el-icon>
   </el-icon>
 
 
