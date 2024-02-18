e9fec74c4323d3da8fd81d9bbd694a9fbe1a3ebc
sakai <sakai263016@gmail.com>
2024-02-18 15:54:14 +0800

    v2配置路径

--- v2/src/router/index.js
+++ v2/src/router/index.js
@@ -5,15 +5,25 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+        component: HomeView,
+        children: [
+            {
+                path: '/reg',
+                component: () => import('../views/home/RegView.vue')
+            },
+            {
+                path: 'login',
+                component: () => import('../views/home/LoginView.vue')
+            },
+            {
+                path: 'index',
+                component: () => import('../views/home/IndexView.vue')
+            },
+            {
+                path: 'detail',
+                component: () => import('../views/home/DetailView.vue')
+            }
+        ]
     }
 ]
 
