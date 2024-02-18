d25d42d074db8d71f98a1b4a18b5e9bdee3c3b88
sakai <sakai263016@gmail.com>
2024-02-18 11:40:09 +0800

    员工列表综合练习

--- v1/src/views/EmployeesList.vue
+++ v1/src/views/EmployeesList.vue
@@ -2,13 +2,16 @@
 import {ref} from "vue";
 
 const tableColumns = ref([
-  {prop: 'name', lable: '姓名'},
-  {prop: 'salary', lable: '工资'},
-  {prop: 'job', lable: '岗位'},
+  {type: 'index', label: '编号', width: 80},
+  {prop: 'name', label: '姓名'},
+  {prop: 'salary', label: '工资'},
+  {prop: 'job', label: '岗位'},
 ]);
 
+//所有员工数组
 const arr = ref([])
 
+//新添加员工数组
 const emp = ref({
   name: '', salary: '', job: ''
 })
@@ -16,6 +19,10 @@ const emp = ref({
 //定义函数
 const add = () => {
   arr.value.push({...emp.value})
+}
+
+const del = ($index, row) => {
+
 }
 </script>
 
@@ -36,8 +43,11 @@ const add = () => {
     </el-form-item>
   </el-form>
   <el-table :data="arr" style="width: 600px;margin: 0 auto">
-    <el-table-column v-for="item in tableColumns">
-
+    <el-table-column v-for="item in tableColumns" v-bind="item"></el-table-column>
+    <el-table-column label="操作">
+      <template #default="scope">
+        <el-button type="danger" @click="del(scope.$index,scope.row)">Delete</el-button>
+      </template>
     </el-table-column>
   </el-table>
 </template>
