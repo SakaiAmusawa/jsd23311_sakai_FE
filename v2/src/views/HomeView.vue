1f5eef84a1251cedd2cc2e9abe17451f8e9ba3aa
sakai <sakai263016@gmail.com>
2024-02-18 15:43:34 +0800

    v2初始化改造

--- v2/src/views/HomeView.vue
+++ v2/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
-      <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
+      <el-header>共有的头部区域</el-header>
+      <el-main>
+        <!--   主体部分是可变区域     -->
+        <router-view></router-view>
+      </el-main>
+      <el-footer>共有的尾部区域</el-footer>
     </el-container>
   </div>
 </template>
---
+++ v2/src/views/home/DetailView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>详情页</h1>
+</template>
+
+<style scoped>
+
+</style>
