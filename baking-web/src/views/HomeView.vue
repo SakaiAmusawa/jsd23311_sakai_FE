0a8e762b7c52c517c89814f6f69cec030fd261fb
sakai <sakai263016@gmail.com>
2024-02-19 10:32:12 +0800

    共有尾部部分

--- baking-web/src/views/HomeView.vue
+++ baking-web/src/views/HomeView.vue
@@ -42,10 +42,52 @@
           </el-col>
         </el-row>
       </el-header>
+
       <el-main>
+        <!--可变区域-->
         <router-view/>
       </el-main>
-      <el-footer>Footer</el-footer>
+
+      <el-footer style="background-color:rgb(50,50,50);color: #666;height: 300px;padding: 50px;text-align: center">
+        <!--尾部-->
+        <div style="width: 1200px;margin: 0 auto;">
+          <el-row :gutter="10">
+            <el-col :span="8">
+              <img src="/imgs/icon.png" alt="LOGO">
+              <p>教程灵感就看烘焙坊</p>
+              <p>烘焙行业网络社区平台</p>
+              <p>全国成百上千个职位等你来</p>
+            </el-col>
+            <el-col :span="8" id="footer-center">
+              <el-row :gutter="10">
+                <el-col :span="8">
+                  <h3>关于我们</h3>
+                  <p>烘焙学院</p>
+                  <p>烘焙食谱</p>
+                  <p>分类信息</p>
+                  <p>求职招聘</p>
+                  <p>社区交流</p>
+                </el-col>
+                <el-col :span="8">
+                  <h3>服务与支持</h3>
+                  <p>联系我们</p>
+                  <p>广告投放</p>
+                  <p>用户协议</p>
+                  <p>友情链接</p>
+                  <p>在线反馈</p>
+                </el-col>
+                <el-col :span="8">
+                  <h3>底部导航</h3>
+                  <p>Archiver</p>
+                  <p>手机版</p>
+                  <p>小黑屋</p>
+                </el-col>
+              </el-row>
+            </el-col>
+            <el-col :span="8">10</el-col>
+          </el-row>
+        </div>
+      </el-footer>
     </el-container>
   </div>
 </template>
@@ -53,3 +95,12 @@
 <script setup>
 import {Search} from '@element-plus/icons-vue'
 </script>
+
+<style>
+footer h3{
+  color: #fff;
+}
+#footer-center p {
+  margin: 5px 0;
+}
+</style>
