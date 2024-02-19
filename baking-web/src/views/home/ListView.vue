060f76848a6605c3469ac6d6dbd7aa5d8976775e
sakai <sakai263016@gmail.com>
2024-02-19 15:05:53 +0800

    列表页面

--- baking-web/src/views/home/ListView.vue
+++ baking-web/src/views/home/ListView.vue
@@ -3,7 +3,26 @@
 </script>
 
 <template>
-  <h1>列表页</h1>
+  <div style="width: 1200px;margin: 0 auto;">
+    <el-row :gutter="10" style="margin-top: 5px;">
+      <el-col :span="6" v-for="item in 4">
+        <el-card class="box-card">
+          <router-link to="/detail" style="text-decoration: none">
+            <img src="/imgs/a.jpg" alt="" style="width: 100%;height: 150px;">
+            <p style="height: 40px;">奶油面包</p>
+          </router-link>
+          <el-row :gutter="10" style="margin-top: 5px;">
+            <el-col :span="4">
+              <el-avatar src="/imgs/head.jpg"></el-avatar>
+            </el-col>
+            <el-col :span="16" style="font-size: 20px;line-height: 40px">传奇哥</el-col>
+            <el-col :span="4" style="line-height: 40px">欧包</el-col>
+          </el-row>
+        </el-card>
+      </el-col>
+    </el-row>
+
+  </div>
 </template>
 
 <style scoped>
