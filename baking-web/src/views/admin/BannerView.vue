e1c3e120c72f4bda7aa5aee98e2ddc41d9413f57
sakai <sakai263016@gmail.com>
2024-02-20 11:24:19 +0800

    轮播图管理页面完成

--- baking-web/src/views/admin/BannerView.vue
+++ baking-web/src/views/admin/BannerView.vue
@@ -1,9 +1,39 @@
 <script setup>
 
-</script>
+import {ref} from "vue";
+import {ElMessage} from "element-plus";
 
+const arr = ref([
+  {imgUrl: '/imgs/banner1.jpg', createTime: '2024年02月20日 10:48:50'},
+  {imgUrl: '/imgs/banner2.jpg', createTime: '2024年02月20日 10:48:50'},
+  {imgUrl: '/imgs/banner3.jpg', createTime: '2024年02月20日 10:48:50'},
+])
+
+const del = (i) => {
+  if (confirm('确认删除？')) {
+    arr.value.splice(i, 1)
+    ElMessage.success('删除成功')
+  }
+}
+
+</script>
+<!--轮播图管理页面-->
 <template>
-  <h1>轮播图管理页</h1>
+  <el-table :data="arr">
+    <!--                                                     align使得元素居中     -->
+    <el-table-column type="index" label="编号" width="100px" align="center"></el-table-column>
+    <el-table-column label="轮播图" width="450px" align="center">
+      <template #default="scope">
+        <img :src="scope.row.imgUrl" alt="" style="width: 100%;">
+      </template>
+    </el-table-column>
+    <el-table-column label="创建时间" prop="createTime" align="center"></el-table-column>
+    <el-table-column label="操作" align="center">
+      <template #default="scope">
+        <el-button type="danger" @click="del(scope.$index)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <style scoped>
