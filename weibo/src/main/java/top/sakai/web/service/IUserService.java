f0a678270402f426ebce9b79d15cb626e6bea252
sakai <sakai263016@gmail.com>
2024-02-21 11:53:24 +0800

    对UserController改造

--- weibo/src/main/java/top/sakai/web/service/IUserService.java
+++ weibo/src/main/java/top/sakai/web/service/IUserService.java
@@ -1,8 +1,10 @@
 package top.sakai.web.service;
 
+import org.springframework.stereotype.Service;
 import top.sakai.web.pojo.dto.UserLoginDTO;
 import top.sakai.web.pojo.dto.UserRegDTO;
 
+@Service
 public interface IUserService {
     void reg(UserRegDTO userRegDTO);
 
