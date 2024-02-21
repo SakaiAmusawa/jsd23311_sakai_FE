8d1c53511d90867713a3a541cb451cbc2173a2e4
sakai <sakai263016@gmail.com>
2024-02-21 11:13:10 +0800

    定义查询方法和插入方法

--- weibo/src/main/java/top/sakai/web/mapper/UserMapper.java
+++ weibo/src/main/java/top/sakai/web/mapper/UserMapper.java
@@ -1,7 +1,12 @@
 package top.sakai.web.mapper;
 
 import org.springframework.stereotype.Repository;
+import top.sakai.web.pojo.entity.User;
+import top.sakai.web.pojo.vo.UserVO;
 
 @Repository //表明这是一个持久层的接口
 public interface UserMapper {
+    UserVO selectByUserName(String username);//定义查询方法
+
+    int insert(User user);//定义插入方法
 }
