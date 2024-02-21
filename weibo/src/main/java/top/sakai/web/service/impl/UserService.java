d39d225e8191ccfba61eef419ce766b02e0a9892
sakai <sakai263016@gmail.com>
2024-02-21 12:02:14 +0800

    三层架构改造

--- weibo/src/main/java/top/sakai/web/service/impl/UserService.java
+++ weibo/src/main/java/top/sakai/web/service/impl/UserService.java
@@ -1,18 +1,33 @@
 package top.sakai.web.service.impl;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import top.sakai.web.mapper.UserMapper;
 import top.sakai.web.pojo.dto.UserLoginDTO;
 import top.sakai.web.pojo.dto.UserRegDTO;
+import top.sakai.web.pojo.entity.User;
+import top.sakai.web.pojo.vo.UserVO;
+import top.sakai.web.response.StatusCode;
 import top.sakai.web.service.IUserService;
 
+import java.util.Date;
+
+@Service
 public class UserService implements IUserService {
     @Autowired
     UserMapper userMapper;
 
     @Override
     public void reg(UserRegDTO userRegDTO) {
-
+        UserVO userVO = userMapper.selectByUserName(userRegDTO.getUsername());
+        if (userVO != null) {
+            throw new ServiceException(StatusCode.USERNAME_ALREADY_EXISTS);
+        }
+        User user = new User();
+        BeanUtils.copyProperties(userRegDTO, user);
+        user.setCreated(new Date());
+        userMapper.insert(user);
     }
 
     @Override
