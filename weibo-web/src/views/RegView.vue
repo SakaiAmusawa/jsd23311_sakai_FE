c88a59d3f4d9218850ba5df74d8887f44cfb6f56
sakai <sakai263016@gmail.com>
2024-02-21 09:43:48 +0800

    微博项目注册页前端:声明一个响应式对象，将输入框进行双向绑定

--- weibo-web/src/views/RegView.vue
+++ weibo-web/src/views/RegView.vue
@@ -3,7 +3,16 @@
   <el-card style="width: 450px;margin: 0 auto;padding: 20px 10px 0 0;">
     <el-form label-width="60px">
       <el-form-item label="用户名">
-        <el-input placeholder="请输入用户名"></el-input>
+        <el-input v-model="user.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="user.password" placeholder="请输入密码" type="password"></el-input>
+      </el-form-item>
+      <el-form-item label="昵称">
+        <el-input v-model="user.nickname" placeholder="请输入昵称"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" style="width: 100%;">注册</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -11,8 +20,18 @@
 
 <script setup>
 
+import {ref} from "vue";
+
+const user = ref({
+  username: '',
+  password: '',
+  nickname: ''
+})
+
 </script>
 
 <style scoped>
-
+::v-deep(*.el-input__wrapper) {
+  background-color: transparent;
+}
 </style>
