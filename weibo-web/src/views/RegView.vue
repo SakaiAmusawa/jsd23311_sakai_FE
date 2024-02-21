7e8d99bfa38205e1738856a70cf2cfc07d78d020
sakai <sakai263016@gmail.com>
2024-02-21 09:52:10 +0800

    微博项目注册页前端:为注册按钮添加点击事件（在控制台打印消息，查看是否获得了输入的文字）

--- weibo-web/src/views/RegView.vue
+++ weibo-web/src/views/RegView.vue
@@ -12,7 +12,7 @@
         <el-input v-model="user.nickname" placeholder="请输入昵称"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" style="width: 100%;">注册</el-button>
+        <el-button type="primary" style="width: 100%;" @click="reg()">注册</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -21,12 +21,18 @@
 <script setup>
 
 import {ref} from "vue";
+import {ElMessage} from "element-plus";
 
 const user = ref({
   username: '',
   password: '',
   nickname: ''
 })
+
+const reg = () => {
+  console.log(user.value)
+  ElMessage.success("注册成功")
+}
 
 </script>
 
