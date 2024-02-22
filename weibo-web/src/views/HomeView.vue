905fe9a603c011bed7031690544f2d70c384dd4c
sakai <sakai263016@gmail.com>
2024-02-22 10:19:12 +0800

    对首页进行改造，用户已登录显示一个页面，未登录显示另外的页面

--- weibo-web/src/views/HomeView.vue
+++ weibo-web/src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <template>
   <h1>首页</h1>
-  <router-link to="/reg">注册</router-link>
-  <router-link to="/login">登录</router-link>
+  <!-- 如果用户信息为空，显示登录注册 -->
+  <div v-if="user==null">
+    <router-link to="/reg">注册</router-link>
+    <router-link to="/login">登录</router-link>
+  </div>
+  <div v-else>
+    <h2>欢迎{{ user.nickname }}回来！</h2>
+    <router-link to="/post">
+      <el-button>发微博</el-button>
+    </router-link>
+    <el-button>退出登录</el-button>
+  </div>
+
+  <!-- 如果用户信息不为空，显示欢迎信息，并显示发微博与退出登录 -->
 </template>
 
 <script setup>
+
+import {ref} from "vue";
+
+const user = ref(localStorage.user ? JSON.parse(localStorage.user) : null)
 
 </script>
