9df71b79f242b6e72628bf5f6736f6dc7d630282
sakai <sakai263016@gmail.com>
2024-02-22 10:54:49 +0800

    初步完成发布微博的功能

--- weibo-web/src/views/PostView.vue
+++ weibo-web/src/views/PostView.vue
@@ -1,13 +1,35 @@
 <script setup>
 
 import {ref} from "vue";
+import router from "@/router";
+import axios from "axios";
+import qs from "qs"
+import {ElMessage} from "element-plus";
 
 const weibo = ref({
   content: '',
 })
 
 const post = () => {
-
+  //得到用户信息
+  let user = localStorage.user ? JSON.parse(localStorage.user) : null
+  //判断是否登录
+  if (user == null) {
+    alert('请登录！')
+    router.push('/login')
+  }
+  //设置作者ID
+  weibo.value.userId = user.id;
+  //将数据转化为查询字符串
+  let data = qs.stringify(weibo.value)
+  axios.post('https://localhost:8080/v1/users/post', data).then((response) => {
+    if (response.data.code === 2001) {
+      ElMessage.success('发布成功')
+      router.push('/')
+    } else {
+      ElMessage.error(response.data.msg)
+    }
+  })
 }
 
 </script>
