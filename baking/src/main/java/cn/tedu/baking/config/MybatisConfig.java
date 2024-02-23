319df64ddb925129f40e9a347cbe0e1c96c844bc
sakai <sakai263016@gmail.com>
2024-02-23 10:58:17 +0800

    添加依赖配置文件

---
+++ baking/src/main/java/cn/tedu/baking/config/MybatisConfig.java
@@ -0,0 +1,9 @@
+package cn.tedu.baking.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("cn.tedu.baking")
+public class MybatisConfig {
+}
