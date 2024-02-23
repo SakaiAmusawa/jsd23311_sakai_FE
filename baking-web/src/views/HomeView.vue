e3aa60eb8e9211bb029491c04c2389feb775d86f
sakai <sakai263016@gmail.com>
2024-02-23 17:13:14 +0800

    实现登录后气泡卡片的变化

--- baking-web/src/views/HomeView.vue
+++ baking-web/src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header style="background-color: #fff;height: 80px;">
+      <el-header style="background-color:#fff;height: 80px;">
         <!--头部-->
         <el-row :gutter="20" style="width: 1200px;margin: 0 auto;">
           <el-col :span="6">
-            <!--头部logo图片-->
-            <router-link to="/"><img alt="LOGO" src="/imgs/icon.png" style="width: 200px;"></router-link>
+            <!--头部Logo图片 点击logo跳转至 http://localhost:8080/index -->
+            <router-link to="/">
+              <img src="/imgs/icon.png" style="width: 200px;">
+            </router-link>
           </el-col>
           <el-col :span="10">
             <!--头部导航栏-->
-            <el-menu active-text-color="orange" mode="horizontal" style="height: 70px;" @select="handleSelect">
+            <el-menu @select="handleSelect" mode="horizontal" active-text-color="orange" style="height: 70px;">
               <el-menu-item index="1">食谱</el-menu-item>
               <el-menu-item index="2">视频</el-menu-item>
               <el-menu-item index="3">资讯</el-menu-item>
@@ -25,40 +27,49 @@
             </el-input>
           </el-col>
           <el-col :span="2">
-            <!--气泡卡片-->
-            <el-popover title="欢迎访问烘焙坊">
+            <!--气泡卡片1-->
+            <el-popover title="欢迎访问烘焙坊" v-if="user==null">
               <template #reference>
-                <router-link to="/personal">
-                  <el-icon color="#666" size="25" style="margin-top: 30px;">
-                    <User/>
-                  </el-icon>
-                </router-link>
+                <el-icon size="25" color="#666" style="margin-top:30px;">
+                  <User/>
+                </el-icon>
               </template>
-              <div style="text-align: center">
-                <el-button size="small" type="info" @click="router.push('/reg')">注册</el-button>
-                <el-button size="small" type="warning" @click="router.push('/login')">登录</el-button>
+              <div style="text-align:center;">
+                <el-button type="info" size="small" @click="router.push('/reg')">注册</el-button>
+                <el-button type="warning" size="small" @click="router.push('/login')">登录</el-button>
+              </div>
+            </el-popover>
+            <!--气泡卡片2-->
+            <el-popover v-else :title="'欢迎'+user.nickname+'回来!'"
+                        :width="200" popper-style="text-align:center;">
+              <template #reference>
+                <el-icon size="25" color="#666" style="margin-top:30px;">
+                  <User/>
+                </el-icon>
+              </template>
+              <div style="text-align:center;">
+                <el-button type="success" size="small" @click="router.push('/personal')">个人中心</el-button>
+                <el-button type="danger" size="small" @click="logout()">退出登录</el-button>
               </div>
             </el-popover>
           </el-col>
         </el-row>
       </el-header>
-
+      <!--首页中部是可变区域-->
       <el-main>
-        <!--可变区域-->
         <router-view/>
       </el-main>
-
-      <el-footer style="background-color:rgb(50,50,50);color: #666;height: 300px;padding: 50px;text-align: center">
-        <!--尾部-->
+      <!--首页尾部-->
+      <el-footer style="background-color:rgb(50,50,50);color:#666;height:300px;padding:50px;text-align:center;">
         <div style="width: 1200px;margin: 0 auto;">
           <el-row :gutter="10">
             <el-col :span="8">
-              <img alt="LOGO" src="/imgs/icon.png">
+              <img src="/imgs/icon.png">
               <p>教程灵感就看烘焙坊</p>
               <p>烘焙行业网络社区平台</p>
               <p>全国成百上千个职位等你来</p>
             </el-col>
-            <el-col id="footer-center" :span="8">
+            <el-col :span="8" id="footer-center">
               <el-row :gutter="10">
                 <el-col :span="8">
                   <h3>关于我们</h3>
@@ -85,7 +96,9 @@
               </el-row>
             </el-col>
             <el-col :span="8">
-              <p style="font-size: 60px;color: #666666;margin: 10px 0 ;"><span style="color: orange">烘焙</span>坊</p>
+              <p style="font-size:60px;color:#666;margin:10px 0">
+                <span style="color:orange;">烘焙</span>坊
+              </p>
               <p>烘焙行业社区平台</p>
             </el-col>
           </el-row>
@@ -96,8 +109,19 @@
 </template>
 
 <script setup>
-import {Search} from '@element-plus/icons-vue'
+import {Search} from '@element-plus/icons-vue';
 import router from "@/router";
+import {ref} from "vue";
+
+const user = ref(localStorage.user ? JSON.parse(localStorage.user) : null);
+//实现退出功能
+const logout = () => {
+  if (confirm("您确认要退出吗?")) {
+    localStorage.clear();
+    user.value = null;
+    router.push('/');
+  }
+}
 
 const handleSelect = (key, keyPath) => {
   router.push('/list')
--- baking-web/src/views/home/LoginView.vue
+++ baking-web/src/views/home/LoginView.vue
@@ -1,62 +1,54 @@
-<script setup>
-
-import {ref} from "vue";
-import qs from "qs"
-import axios from "axios";
-import {ElMessage} from "element-plus";
-import router from "@/router";
-
-const user = ref({username: '', password: ''})
-
-const login = () => {
-  let data = qs.stringify(user.value)
-  axios.post('http://localhost:8080/v1/users/login', data).then(
-      (response) => {
-        if (response.data.code === 2001) {
-          ElMessage.success('登录成功')
-          localStorage.setItem('user', JSON.stringify(user))
-          router.push('/index')
-        } else {
-          ElMessage.error(response.data.msg)
-        }
-      }
-  )
-}
-
-</script>
-
+<!--登录页-->
 <template>
   <div id="main-div">
-    <!--                                                                        设置距离原来自己的顶部位置有100像素  -->
-    <el-card class="box-card" style="width: 500px ;height: 300px;margin: 0 auto;position:relative;top: 100px">
+    <el-card style="width:500px;height:300px;
+    margin:0 auto;position:relative;top:100px;">
       <el-form label-width="60px">
-        <el-form-item label="用户名" style="margin-top: 60px;">
+        <el-form-item label="用户名" style="margin-top:60px;">
           <el-input placeholder="请输入用户名" v-model="user.username"></el-input>
         </el-form-item>
         <el-form-item label="密码">
-          <el-input placeholder="请输入密码" type="password" v-model="user.password"></el-input>
+          <el-input type="password" placeholder="请输入密码" v-model="user.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button style="width: 100%;" type="primary" @click="login()">登录</el-button>
+          <el-button type="primary" style="width:100px;margin-left: 120px;" @click="login()">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
   </div>
 </template>
 
+<script setup>
+import {ref} from "vue";
+import qs from "qs";
+import axios from "axios";
+import {ElMessage} from "element-plus";
+import router from "@/router";
+
+const user = ref({username:'',password:''});
+const login = ()=>{
+  let data = qs.stringify(user.value);
+  axios.post('http://localhost:8080/v1/users/login',data)
+      .then((response)=>{
+        if(response.data.code==2001){
+          ElMessage.success('登录成功!');
+          let user = response.data.data;
+          localStorage.user= JSON.stringify(user);
+          //router.push('/');
+          location.href='/';//登录成功存入用户信息后，立即自动刷新页面
+        }else{
+          ElMessage.error(response.data.msg);
+        }
+      })
+}
+</script>
+
 <style scoped>
 #main-div {
   height: 500px;
-  background-image: url("/public/imgs/loginbg.gif");
+  background-image: url('/public/imgs/loginbg.gif');
+  /*常用于设置封面*/
   background-size: cover;
   background-position: center;
-
-  .box-card {
-    background-color: transparent;
-  }
-
-  ::v-deep(*.el-input__wrapper) {
-    background-color: transparent;
-  }
 }
 </style>
