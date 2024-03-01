5e4e479eccefbc460c5b0d8f930e4a0fa894b518
sakai <sakai263016@gmail.com>
2024-03-01 10:43:34 +0800

    评论展示列表前后端:优化了时间显示的格式

--- baking/src/main/java/cn/tedu/baking/pojo/vo/CommentVO.java
+++ baking/src/main/java/cn/tedu/baking/pojo/vo/CommentVO.java
@@ -11,6 +11,6 @@ public class CommentVO {
     private String userImgUrl;
     private String nickname;
     private String content;
-    @JsonFormat(pattern = "yyyy/MM/dd", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 }
